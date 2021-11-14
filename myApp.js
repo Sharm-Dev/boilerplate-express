ae0a295ea9bd02ea1a96b23d6c7cc92ac83d0656
Sharm-Dev <sharmainetaylor@ymail.com>
2021-11-14 18:18:38 +0000

    app.use gives access to files in the public folder so we can access all the files in there

--- myApp.js
+++ myApp.js
@@ -2,16 +2,10 @@ const express = require("express");
 const app = express();
 let absolutePath = __dirname + "/views/index.html";
 
-//console logging a string
-// console.log('Hello World');
-
 /*
-Use the app.get() method to serve the string "Hello Express" to GET requests matching the / (root) path.
-  
-app.get("/", (req, res) => {
-    res.send("Hello Express");
-});  
+app.use gives access to files in the public folder as declared  below
 */
+app.use("/public", express.static(__dirname + "/public"));
 
 /*
 Made a get request to the html file and also imported the html file into this file with a variable at the top of page 
