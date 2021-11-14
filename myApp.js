f11fda3eae602cafbae68687b02964dceef91961
Sharm-Dev <sharmainetaylor@ymail.com>
2021-11-14 17:57:05 +0000

    add get request to the html file and also imported the html file into this file with a variable at the top of page

--- myApp.js
+++ myApp.js
@@ -1,47 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+let absolutePath = __dirname + "/views/index.html";
 
-
-
-app.get("/", (req, res) => {
-    res.send("Hello Express");
-})
-
+//console logging a string
 // console.log('Hello World');
 
+/*
+Use the app.get() method to serve the string "Hello Express" to GET requests matching the / (root) path.
+  
+app.get("/", (req, res) => {
+    res.send("Hello Express");
+});  
+*/
 
+/*
+Made a get request to the html file and also imported the html file into this file with a variable at the top of page 
+*/
+app.get("/", (req, res) => {
+  res.sendFile(absolutePath);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
